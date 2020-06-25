863bc2a15ec432e680b2a4571cda0fec8528abfb
hilllis <63603525+hilllis@users.noreply.github.com>
2020-06-25 20:23:59 +0900

    프로필 창에서 내 이름, 내 한줄 소개 업데이트

--- Mustagram2/Mustagram2/profile.cs
+++ Mustagram2/Mustagram2/profile.cs
@@ -12,11 +12,27 @@ namespace Mustagram2
 {
     public partial class profile : UserControl
     {
+        MustagramClient client = MustagramClient.GetClient();
+        Set_User setuser = Set_User.SetUser();
         public profile()
         {
             InitializeComponent();
-        
-
+            string User_ID = setuser.getUser_id();
+            string userIntro = "";
+            pro_ID.Text = User_ID.ToString();
+            Func<Task> runAsync = async () =>
+            {
+                try
+                {
+                    userIntro = await client.GetPersonalDescription(User_ID).ConfigureAwait(false);
+                }
+                catch (Exception q)
+                {
+                    Console.WriteLine(q.Message);
+                }
+            };
+            runAsync().GetAwaiter().GetResult();
+            pro_Name.Text = userIntro.ToString();
          }
 
         private void panel6_Paint(object sender, PaintEventArgs e)
